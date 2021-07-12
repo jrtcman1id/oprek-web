33e9528b41422c607c532d9a9ec7625a4c4930ea
Adam Mubarok <adammubarok@gmail.com>
2021-07-12 20:11:53 +0700

    fix svg by adding xmlns attribute, enable custom element as component

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,10 @@ module.exports = {
     presets: [
         '@babel/preset-env',
         [ '@babel/preset-react', {
+            "throwIfNamespace": false, // defaults to true
             runtime: 'automatic',
-            importSource: '@vanilla-jsx'
+            importSource: '@bawailmu'
         } ]
     ],
-    plugins: [
-        'transform-class-properties'
-    ]
+    plugins: []
 }
